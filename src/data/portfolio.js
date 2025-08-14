45c3ff86bbb2684c3105e9bc593fdde7850a929e
N1025684 <alex.farranjones2024@my.ntu.ac.uk>
2025-08-14 18:04:44 +0100

    added pokemon finder in portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -53,4 +53,16 @@ export default [
         FE_Repo:"https://github.com/RTXVader/Pokedex-GUI/blob/main/main.py",
         BE_Repo:""
     },
+    {
+        title: 'Pokemon Finder',
+        imgURL: '/assets/Screenshot 2024-04-10 at 15.50.33.png',
+        stack: ['Python'],
+        link:'https://github.com/RTXVader/Pokedex-GUI/blob/main/main.py',
+        description:"The Pokémon Finder GUI is a Python application that allows users to search for information about Pokémon using a graphical user interface (GUI). Built with Python's Tkinter library, this project provides a visually appealing and intuitive interface for users to explore details about various Pokémon.",
+        FE_Repo:"https://github.com/RTXVader/Pokedex-GUI/blob/main/main.py",
+        BE_Repo:""
+    },
+    {
+
+    },
 ]
